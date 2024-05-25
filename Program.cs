d121c99ef090d10370433f39d21c908db6ceb2cc
Maksim Vlasov <54456828+Mask22446@users.noreply.github.com>
2024-05-25 19:20:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void WriteSome() { Console.WriteLine(666); }
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            x = y = z =100;
+            Car colorOfCar = new Car();
+            Console.WriteLine(x + y + z);
+            WriteSome();
+            Console.WriteLine(colorOfCar.color);
+            Console.ReadLine();
+        }
+    }
+}
